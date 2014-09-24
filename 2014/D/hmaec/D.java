8f728d6f10907119236996c0d30dd28cf20982f1
hmaec21 <hmaec21@gmail.com>
2014-09-24 19:07:57 +0900

    2014のDを改良

--- 2014/D/hmaec/D.java
+++ 2014/D/hmaec/D.java
@@ -12,57 +12,53 @@ public class D {
 				break;
 			}
 			sArray = new ArrayList<String>();
+			char[] sca = s.toCharArray();
+			//System.out.println(new String(sca));
 			ans = 0;
-			decode(s, s.length() - 1);
+			decode(sca, 0);
 			//encode();
-			Collections.sort(sArray);
 			System.out.println(ans);
-			if (ans > 0) {
-				if (10 >= ans) {
-					for (int i = 0; i < sArray.size(); i++) {
-						System.out.println(sArray.get(i));
-					}
-				} else {
-					for (int i = 0; i < 5; i++) {
-						System.out.println(sArray.get(i));
-					}
-					for (int i = 5; i > 0; i--) {
-						System.out.println(sArray.get(sArray.size() - i));
-					}
-				}
+			for (int i = 0; i < sArray.size(); i++) {
+				System.out.println(sArray.get(i));
 			}
 		}
 	}
 	
-	public static void decode(String sa, int pos) {
-		StringBuffer sb = new StringBuffer(sa);
-		if (pos == -1) {
-			if (encode(sb.toString())) {
+	public static void decode(char[] sca, int pos) {
+		//System.out.println(new String(sca) + " " + sca[0] + " " + pos + " " + sca.length);
+		if (pos == sca.length) {
+			if (encode(sca)) {
 				ans++;
-				sArray.add(sb.toString());
+				sArray.add(new String(sca));
+				Collections.sort(sArray);
+				if (sArray.size() > 10) {
+					sArray.remove(5);
+				}
 			}
 			return;
 		}
-		decode(sb.toString(), pos - 1);
-		if (sb.charAt(pos) != 'z') {
-			sb.setCharAt(pos, (char)(sb.charAt(pos) + 1));
-			decode(sb.toString(), pos - 1);
+		decode(sca, pos + 1);
+		if (sca[pos] != 'z') {
+			//System.out.println(sca[pos]);
+			sca[pos]++;
+			decode(sca, pos + 1);
+			sca[pos]--;
 		}
 	}
 	
-	public static boolean encode(String sa) {
-		StringBuffer sb = new StringBuffer(sa);
+	public static boolean encode(char[] sca) {
+		char[] tmp = Arrays.copyOf(sca, sca.length);
 		for (int i = 1; i <= 25; i++) {
 			char c = (char)('a' + i);
-			String tmp = Character.toString(c);
-			//System.out.println(tmp);
-			int pos = sb.indexOf(tmp);
-			if (pos != -1) {
-				sb.setCharAt(pos, (char)(c - 1));
+			for (int j = 0; j < tmp.length; j++) {
+				if (tmp[j] == c) {
+					tmp[j]--;
+					break;
+				}
 			}
 		}
-		//System.out.println(sb);
-		if (s.equals(sb.toString())) {
+		//System.out.println(sca);
+		if (s.equals(new String(tmp))) {
 			return true;
 		} else {
 			return false;
