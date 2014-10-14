ad67eef81dd13fac488d6345064c688aedce028f
hmaec21 <7753489+hmaec21@users.noreply.github.com>
2014-10-14 13:49:28 +0900

    2014のBを書き直し
    AOJに提出したものと同じコード(Dも同じ)

--- 2014/B/hmaec/B.java
+++ 2014/B/hmaec/B.java
@@ -1,105 +1,96 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class B {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		ArrayList<Integer>[] field;
-		while (true) {
-			int h = sc.nextInt();
-			if (h == 0) {
-				break;
-			}
-			field = new ArrayList[5];
-			field[0] = new ArrayList();
-			field[1] = new ArrayList();
-			field[2] = new ArrayList();
-			field[3] = new ArrayList();
-			field[4] = new ArrayList();
-			for (int i = 0; i < h; i++) {
-				for (int j = 0 ; j < 5; j++) {
-					int n = sc.nextInt();
-					field[j].add(0, n);
-				}
-			}
-			
-			int score = 0;
-			boolean flag;			
-			do {
-				flag = false;
-				//System.out.println("+");
-				for (int i = h - 1; i >= 0; i--) {
-					//System.out.println("!");
-					int[] tmp = new int[5];
-					if (field[0].size() > i) { 
-						tmp[0] = field[0].get(i);
-					}
-					if (field[1].size() > i) {
-						tmp[1] = field[1].get(i);
-					}
-					if (field[2].size() > i) {
-						tmp[2] = field[2].get(i);
-					}
-					if (field[3].size() > i) {
-						tmp[3] = field[3].get(i);
-					}
-					if (field[4].size() > i) {
-						tmp[4] = field[4].get(i);
-					}
-					if (tmp[0] == tmp[1] && tmp[1] == tmp[2] && tmp[2] == tmp[3] && tmp[3] == tmp[4] && tmp[0] != 0) {
-						field[0].remove(i);
-						field[1].remove(i);
-						field[2].remove(i);
-						field[3].remove(i);
-						field[4].remove(i);
-						score += tmp[0] * 5;
-						flag = true;
-					} else if (tmp[0] == tmp[1] && tmp[1] == tmp[2] && tmp[2] == tmp[3] && tmp[0] != 0) {
-						field[0].remove(i);
-						field[1].remove(i);
-						field[2].remove(i);
-						field[3].remove(i);
-						score += tmp[0] * 4;
-						flag = true;
-					} else if (tmp[1] == tmp[2] && tmp[2] == tmp[3] && tmp[3] == tmp[4] && tmp[1] != 0) {
-						field[1].remove(i);
-						field[2].remove(i);
-						field[3].remove(i);
-						field[4].remove(i);
-						score += tmp[1] * 4;
-						flag = true;
-					} else if (tmp[0] == tmp[1] && tmp[1] == tmp[2] && tmp[0] != 0) {
-						field[0].remove(i);
-						field[1].remove(i);
-						field[2].remove(i);
-						score += tmp[0] * 3;
-						flag = true;
-					} else if (tmp[1] == tmp[2] && tmp[2] == tmp[3] && tmp[1] != 0) {
-						field[1].remove(i);
-						field[2].remove(i);
-						field[3].remove(i);
-						score += tmp[1] * 3;
-						flag = true;						
-					} else if (tmp[2] == tmp[3] && tmp[3] == tmp[4] && tmp[2] != 0) {
-						field[2].remove(i);
-						field[3].remove(i);
-						field[4].remove(i);
-						score += tmp[2] * 3;
-						flag = true;
-					}
-					
-				}
-			} while (flag);
-			System.out.println(score);
-			
-			/*
-			for (int i = 0; i < h; i++) {
-				for (int j = 0; j < 5; j++) {
-					System.out.print(field[j].get(i));
-				}
-				System.out.println();
-			}
-			System.out.println();
-			*/
-		}
-	}
+    static int sum; // 消した石の数値の合計
+    public static void main(String[] args) {
+        final int ROW = 5; // 列の数
+        final int MINREMOVE = 3; // 消すのに必要な個数
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            int H = sc.nextInt();
+            if (H == 0) {
+                break;
+            }
+            
+            int[][] field = new int[H][ROW+1];
+            for (int i = 0; i < H; i++) {
+                for (int j = 0; j < ROW; j++) {
+                    field[i][j] = sc.nextInt();
+                }
+                field[i][ROW] = 0;
+            }
+            sum = 0;
+            
+            //printField(field);
+            //System.out.println();
+            while (removeField(field, MINREMOVE)) {
+                //printField(field);
+                //System.out.println();
+                fallField(field);
+                //printField(field);
+                //System.out.println();
+            }
+            
+            System.out.println(sum);
+        }
+    }
+    
+    public static boolean removeField(int[][] field, int n) {
+        boolean isRemove = false; // 1回以上消したかどうか
+        
+        for (int i = 0; i < field.length; i++) {
+            int num = 0; // num連続
+            for (int j = 0; j < field[i].length; j++) {
+                if (j == 0) {
+                    num = 1;
+                } else {
+                    //System.out.println(i + " " + j);
+                    if (field[i][j-1] == field[i][j] && field[i][j] != 0) {
+                        num++;
+                    } else {
+                        if (num >= n) {
+                            removeStone(field, j - num, i, num);
+                            isRemove = true;
+                        }
+                        num = 1;
+                    }
+                }
+            }
+        }
+        return isRemove;
+    }
+    
+    public static void removeStone(int[][] field, int x, int y, int n) {
+        for (int i = 0; i < n; i++) {
+            sum += field[y][x+i];
+            field[y][x+i] = 0;
+        }
+    }
+    
+    public static void fallField(int[][] field) {
+        // 下から2段目以上にある石を落とす
+        for (int i = field.length - 2; i >= 0; i--) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j] != 0) {
+                    int x = j;
+                    int y = i;
+                    while (y < field.length - 1&& field[y+1][x] == 0) {
+                        field[y+1][x] = field[y][x];
+                        field[y][x] = 0;
+                        y++;
+                    }
+                }
+            }
+        }
+    }
+    
+    public static void printField(int[][] field) {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print(field[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
